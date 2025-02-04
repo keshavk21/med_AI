08f9306ccbb82ec49d8655aa2049acf925d0d136
keshavk21 <khandelwalkeshav2004@gmail.com>
2025-02-04 11:45:06 +0530

    api final part 2

--- menu/duplication/classifier.py
+++ menu/duplication/classifier.py
@@ -10,12 +10,11 @@ google_api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=google_api_key)
 
 # Define system prompt
-system_prompt_tdc ={"text":"""You are a medical AI Agent.Classify on the bases:
+system_prompt_tdc ={"text":"""You are a medical AI Agent.Classify theraputic duplcation on the bases:
                 1.Major:Highly clinically significant. Avoid combinations; the risk of the interaction outweighs the benefit.
                 2.Moderate:Moderately clinically significant. Usually avoid combinations; use it only under special circumstances.
                 3.Minor:Minimally clinically significant. Minimize risk; assess risk and consider an alternative drug, take steps to circumvent the interaction risk and/or institute a monitoring plan.
-                4.Unknown:Unknown interaction information available./n
-                answer in 1 word"""}
+                in 1 word"""}
 
 # Function to call Gemini API
 def message_gemini(user_prompt):
